9815528dfa09b07674f5b5abd918c742a23d0f44
ops10ops <89559438+ops10ops@users.noreply.github.com>
2022-08-29 23:28:02 +1000

    1 add a folder: Startup 2 add a class: DependencyInjectionSetup.cs 3 make DependencyInjectionSetup static 4 create a method RegisterServices 5 make RegisterServices static 6 make RegisterServices an extension method by using this keyword 7 pass and return IServiceCollection

---
+++ CleanProgramCs/Startup/DependencyInjectionSetup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanProgramCs.Startup
+{
+    public static class DependencyInjectionSetup
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
